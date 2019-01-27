eb6d2a6fdc8e7caaf462c85c0449beadd11f17b4
Sorina <chirila_sorina@yahoo.com>
2019-01-27 11:30:25 +0200

    first commit

---
+++ src/main/java/Darts.java
@@ -0,0 +1,13 @@
+public class Darts {
+
+    public  enum Multiplier {
+        DOUBLE, TRIPLE, XX;
+    }
+
+    private int score = 301;
+    private int dartsLeft = 3; //dupa fiecare turn se decrementeaza
+    private boolean isFinished = false;
+    private int turn = 1;//maxim 20 de turnuri
+    private int lastTurnScore = score;//cum se reseteaza scorul curent
+
+}
---
+++ src/test/java/DartsTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class DartsTest {
+
+    private Darts dartBoard;
+
+    @Before
+    public void setUpGame(){
+        dartBoard = new Darts();
+    }
+}
